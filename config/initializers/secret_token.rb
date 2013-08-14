e2b8bacc64c548cdde02c5823da418cb7e6f7775
App Academy Student <student@appacademy.io>
2013-08-14 10:17:30 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendCircle::Application.config.secret_token = 'e0f05bfa487d695830dfb2ab06c18e4a0ead73e791a997d74e772979558e71240adcb205f6617697296f52738cce63b58ccfd8dc050a9e4087e4fc1db73376ea'
